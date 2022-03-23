0a445b386d4c70b68b3ce456362f6ae3e9e8b63d
yardasol <oyardas2@illinois.edu>
2022-03-23 13:25:48 -0500

    modify setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-dirs=("ubuntu-post-installer" "terminal-setup" "conda-envs" "latex-note-taking" "ubuntu-nuclear-software-installer")
-indices=(0 1 2 3 4)
+dirs=("ubuntu-post-installer" "terminal-setup" "conda-envs" "profiles")
+indices=(0 1 2 3)
+# dirs=("ubuntu-post-installer" "terminal-setup" "conda-envs" "latex-note-taking" "ubuntu-nuclear-software-installer")
+#indices=(0 1 2 3 4)
 
 for dir in ${dirs[*]}; do
-  cd $dir | source $dir.sh
+  cd $dir | bash $dir.sh
 done
